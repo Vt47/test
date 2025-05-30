a1e8c9105333868cc4bec640097682399965f093
DLinh182 <dieulinh180204@gmail.com>
2025-05-30 14:11:14 +0700

    add ranking

---
+++ smoking/Models/Ranking.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using smoking12.Models;
+
+
+public class Ranking
+{
+
+    [Key]
+    public int Ranking_ID { get; set; }
+    public int Member_ID { get; set; }
+    public string Badge { get; set; }
+    public int Total_score { get; set; }
+
+
+    
+    public virtual Member Member { get; set; }
+}
